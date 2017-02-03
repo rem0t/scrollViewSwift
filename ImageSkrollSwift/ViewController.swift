7376a4bd89701a43bc8571cd9a6117236e5001b7
Vladislav Kalaev <vladislavkalaev@MacBook-Air-Vladislav.local>
2017-02-03 18:00:17 +0300

    Initial Commit

---
+++ ImageSkrollSwift/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ImageSkrollSwift
+//
+//  Created by Vladislav Kalaev on 03.02.17.
+//  Copyright © 2017 Vladislav Kalaev. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
